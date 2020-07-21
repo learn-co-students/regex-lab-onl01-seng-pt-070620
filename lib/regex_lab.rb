69c4b849ae37e7b69f81fbc1041f796c29b50318
Jill Short <jillian.b.short@gmail.com>
2020-07-21 00:02:53 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,10 @@
 def starts_with_a_vowel?(word)
-
+  word.match? /\A[aeiou]/i
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan /^?un/
+  text.scan /$?ing/
 end
 
 def words_five_letters_long(text)
