997cc7e1d3a741504d5eb0f233c15e1dd9f8ebf6
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-21 02:16:05 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,22 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[AEIOU]|^[aeiou]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  array = text.split(" ")
+  array.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  
 end
 
 def valid_phone_number?(phone)
